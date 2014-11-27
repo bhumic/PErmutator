781dc8861c16d9a7b4ae09006f7b40781edc07ab
bhumic <bruno.humic@fer.hr>
2014-11-27 22:46:36 +0100

    Header changed to include the graph object

--- Headers/Permutator.h
+++ Headers/Permutator.h
@@ -1,10 +1,31 @@
 #pragma once
 #include "PEFunctions.h"
+#include "Graph.h"
+#include "distorm.h"
+#include <vector>
+
+#ifdef _WIN32
+	#pragma comment(lib, "Lib\\distorm.lib")
+#endif
+
+#define MAX_INSTRUCTIONS (100)
 
 class Permutator
 {
 public:
-	Permutator(char* inputFileNames);
+	Permutator(std::fstream& hInputFile);
 	~Permutator();
+
+	void CreateGraph();
+
+private:
+	std::fstream* hInputFile;
+	PIMAGE_DOS_HEADER pDosHeader;
+	PIMAGE_NT_HEADERS pNtHeader;
+	DWORD dwFstSctHdrOffset;
+	Graph graph;
+		
+
+	void InitPermutator();
 };
 
