791014ba83ba207053eddbb5bd8ba87568059d9c
bhumic <bruno.humic@fer.hr>
2014-11-28 19:12:08 +0100

    Work on graph representation continued

--- Source/Permutator.cpp
+++ Source/Permutator.cpp
@@ -18,12 +18,7 @@ Permutator::~Permutator()
 void Permutator::CreateGraph()
 {
 	BYTE* sectionData = nullptr;
-	_DecodeResult res;
-	unsigned int decodedInstructionsCount = 0, next;
-	_DecodeType dt = Decode32Bits;
-	_OffsetType offset = 0;
 	PIMAGE_SECTION_HEADER *pSectionHeader = nullptr;
-	_DecodedInst decodedInstructions[MAX_INSTRUCTIONS];
 
 	PIMAGE_SECTION_HEADER ppSectionHeader;
 	pSectionHeader = &ppSectionHeader;
@@ -35,6 +30,8 @@ void Permutator::CreateGraph()
 
 	DWORD dwSectionSize = (*pSectionHeader)->SizeOfRawData;
 
+	// Create Graph
+	_CreateGraph(sectionData, 0, dwSectionSize);
 }
 
 void Permutator::InitPermutator()
@@ -51,5 +48,61 @@ void Permutator::InitPermutator()
 
 	// Find the file offset to the first section header
 	dwFstSctHdrOffset = pDosHeader->e_lfanew + 4 + IMAGE_SIZEOF_FILE_HEADER + pNtHeader->FileHeader.SizeOfOptionalHeader;			
+}
+
+bool Permutator::IsJump(std::string mnemonic)
+{
+	std::vector<std::string> all_jmps{ "JO", "JNO", "JS", "JNS", "JE", "JZ", "JNE", "JNZ", 
+									   "JB", "JNAE", "JC", "JNB", "JAE", "JNC", "JBE", "JNA",
+									   "JA", "JNBE", "JL", "JNGE", "JGE", "JNL", "JLE", "JNG",
+										"JG", "JNLE", "JP", "JPE", "JNP", "JPO", "JCXZ", "JECXZ", "JMP"};
+	return std::find(std::begin(all_jmps), std::end(all_jmps), mnemonic) != all_jmps.end();
+}
+
+void Permutator::_CreateGraph(BYTE* sectionData, _OffsetType blockOffset, DWORD dwSectionSize)
+{
+	_DecodeResult res;
+	unsigned int decodedInstructionsCount = 0, next;
+	_DecodeType dt = Decode32Bits;
+	_OffsetType offset = blockOffset;
+	_OffsetType offsetEnd;
+	_DecodedInst decodedInstructions[MAX_INSTRUCTIONS];
+
+	while (1)
+	{
+		res = distorm_decode(offset, (const unsigned char*)sectionData, dwSectionSize,
+			dt, decodedInstructions, MAX_INSTRUCTIONS, &decodedInstructionsCount);
+		if (res == DECRES_INPUTERR)
+		{
+			free(sectionData);
+			return;
+		}
+
+		for (unsigned int i = 0; i < decodedInstructionsCount; ++i)
+		{
+			std::string mnemonic(reinterpret_cast<char*>(decodedInstructions[i].mnemonic.p));
+			if (IsJump(mnemonic))
+			{
+				offsetEnd = decodedInstructions[i].offset;
+				DWORD blockSize = offsetEnd + decodedInstructions[i].size - offset;
+				Node node;
+
+				node.SetOffset(offset);
+				node.SetInstructions(sectionData + offset, blockSize);
+				
+			}
+		}
+
+		if (res == DECRES_SUCCESS) break; // All instructions were decoded.
+		else if (decodedInstructionsCount == 0) break;
+
+		// Synchronize:
+		next = (unsigned long)(decodedInstructions[decodedInstructionsCount - 1].offset - offset);
+		next += decodedInstructions[decodedInstructionsCount - 1].size;
+		// Advance ptr and recalc offset.
+		sectionData += next;
+		dwSectionSize -= next;
+		offset += next;
+	}
 }
 
