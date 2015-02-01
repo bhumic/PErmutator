ee7361e6911bf676416d0ebc651808d1ee916346
bhumic <bruno.humic@fer.hr>
2015-02-01 17:39:12 +0100

    Exception and error handling added

--- Source/PEFunctions.cpp
+++ Source/PEFunctions.cpp
@@ -21,7 +21,15 @@
 
 void OpenFile(const char *fileName, std::fstream& hFile)
 {
-	hFile.open(fileName, std::ios::in | std::ios::binary);
+	try
+	{
+		hFile.open(fileName, std::ios::in | std::ios::binary);
+	}
+	catch (std::fstream::failure e)
+	{
+		std::cerr << "OpenFile: Error while opening file: " << e.what() << std::endl;
+		return;
+	}
 }
 
 BYTE* LoadSection(std::fstream& hFile, PIMAGE_SECTION_HEADER pSectionHeader)
@@ -29,17 +37,39 @@ BYTE* LoadSection(std::fstream& hFile, PIMAGE_SECTION_HEADER pSectionHeader)
 	unsigned char *buffer;
 
 	buffer = (unsigned char *)malloc(pSectionHeader->SizeOfRawData);
-	hFile.seekg(pSectionHeader->PointerToRawData, std::ios::beg);
+	if (buffer == nullptr)
+	{
+		std::cerr << "LoadSection: Unable to allocate memory, invalid size: " << pSectionHeader->SizeOfRawData << std::endl;
+		return nullptr;
+	}
 
-	hFile.read((char *) buffer, pSectionHeader->SizeOfRawData);
+	try
+	{
+		hFile.seekg(pSectionHeader->PointerToRawData, std::ios::beg);
+		hFile.read((char *)buffer, pSectionHeader->SizeOfRawData);
+	}
+	catch (std::fstream::failure e)
+	{
+		std::cerr << "LoadSection: Unable to read file: " << e.what() << std::endl;
+		return nullptr;
+	}
+	
 
 	return buffer;
 }
 
 BOOL WriteSection(std::ofstream& hFile, PIMAGE_SECTION_HEADER pSectionHeader, unsigned char *buffer)
 {
-	hFile.seekp(pSectionHeader->PointerToRawData, std::ios::beg);
-	hFile.write((char*) buffer, pSectionHeader->SizeOfRawData);
+	try
+	{
+		hFile.seekp(pSectionHeader->PointerToRawData, std::ios::beg);
+		hFile.write((char*)buffer, pSectionHeader->SizeOfRawData);
+	}
+	catch (std::fstream::failure e)
+	{
+		std::cerr << "WriteSection: Error while writing section data: " << e.what() << std::endl;
+		return FALSE;
+	}
 
 	return TRUE;
 }
@@ -51,25 +81,35 @@ LPVOID ReadHeader(std::fstream& hFile, DWORD dwHeaderSize, DWORD dwOffset)
 	pHeader = malloc(dwHeaderSize);
 	if (pHeader == nullptr)
 	{
+		std::cerr << "ReadHeader: Unable to allocate memory" << std::endl;
 		return nullptr;
 	}
 
-	hFile.seekg(dwOffset, std::ios::beg);
-	hFile.read((char*) pHeader, dwHeaderSize);
+	try
+	{
+		hFile.seekg(dwOffset, std::ios::beg);
+		hFile.read((char*)pHeader, dwHeaderSize);
+	}
+	catch (std::fstream::failure e)
+	{
+		std::cerr << "ReadHeader: Error while reading header: " << e.what() << std::endl;
+		return nullptr;
+	}
 
 	return pHeader;
 }
 
-PIMAGE_SECTION_HEADER FindSection(std::fstream hFile, DWORD dwRVA, DWORD dwFstSectionHeader, WORD wNumSections)
+PIMAGE_SECTION_HEADER FindSection(std::fstream& hFile, DWORD dwRVA, DWORD dwFstSectionHeader, WORD wNumSections)
 {
-	PIMAGE_SECTION_HEADER pSectionHeader = NULL;
+	PIMAGE_SECTION_HEADER pSectionHeader = nullptr;
 
 	for (WORD i = 0; i < wNumSections; ++i)
 	{
 		pSectionHeader = (PIMAGE_SECTION_HEADER)ReadHeader(hFile, IMAGE_SIZEOF_SECTION_HEADER, dwFstSectionHeader + i*IMAGE_SIZEOF_SECTION_HEADER);
-		if (pSectionHeader == NULL)
+		if (pSectionHeader == nullptr)
 		{
-			return nullptr;
+			std::cerr << "FindSection: Invalid section header read" << std::endl;
+			continue;
 		}
 
 		if ((dwRVA >= pSectionHeader->VirtualAddress) &&
@@ -79,6 +119,7 @@ PIMAGE_SECTION_HEADER FindSection(std::fstream hFile, DWORD dwRVA, DWORD dwFstSe
 		}
 
 		free(pSectionHeader);
+		pSectionHeader = nullptr;
 	}
 
 	return nullptr;
@@ -93,20 +134,13 @@ BYTE* ReadData(unsigned char*buffer, DWORD dwOffset, DWORD dwSize)
 {
 	unsigned char *data_segment;
 
-	data_segment = (unsigned char*)malloc(sizeof(unsigned char)* dwSize);
-	memset(data_segment, 0, dwSize);
+	data_segment = (unsigned char*)malloc(dwSize);
 	if (data_segment == nullptr)
 	{
+		std::cerr << "ReadData: Unable to allocate memory" << std::endl;
 		return nullptr;
 	}
-
-/*#ifdef _WIN32
-	memcpy_s(data_segment, dwSize, buffer + dwOffset, dwSize);
-#elif __linux__
-	memcpy(data_segment, buffer + dwOffset, dwSize);
-#else
-#error "OS not supported!"
-#endif*/
+	memset(data_segment, 0, dwSize);
 	std::memcpy(data_segment, buffer + dwOffset, dwSize);
 
 	return data_segment;
@@ -137,8 +171,17 @@ BOOL WriteSectionHeader(PIMAGE_SECTION_HEADER pSectionHeader, DWORD dwSectionID,
 	DWORD dwSectionOffset;
 
 	dwSectionOffset = dwFstSctHeaderOffset + IMAGE_SIZEOF_SECTION_HEADER*dwSectionID;
-	hFile.seekp(dwSectionOffset, std::ios::beg);
-	hFile.write((char*)pSectionHeader, IMAGE_SIZEOF_SECTION_HEADER);
+
+	try
+	{
+		hFile.seekp(dwSectionOffset, std::ios::beg);
+		hFile.write((char*)pSectionHeader, IMAGE_SIZEOF_SECTION_HEADER);
+	}
+	catch (std::fstream::failure e)
+	{
+		std::cerr << "WriteSectionHeader: Error while writing the section header: " << e.what() << std::endl;
+		return FALSE;
+	}
 
 	return TRUE;
 }
@@ -154,20 +197,20 @@ PIMAGE_SECTION_HEADER AddSection(std::fstream& hFile, unsigned char *sectionData
 		dwFstSectionHeaderOffset + IMAGE_SIZEOF_SECTION_HEADER*(pNtHeader->FileHeader.NumberOfSections - 1));
 	if (pLastSectionHeader == nullptr)
 	{
+		std::cerr << "AddSection: Error while reading the last section header" << std::endl;
 		return nullptr;
 	}
 
 	// Allocate memory for new section header
 	pSectionHeader = (PIMAGE_SECTION_HEADER)malloc(IMAGE_SIZEOF_SECTION_HEADER);
+	if (pSectionHeader == nullptr)
+	{
+		std::cerr << "AddSection: Error while allocating memory for new section header" << std::endl;
+		return nullptr;
+	}
 
 	// Populate the section header
-#ifdef _WIN32
-	memcpy_s(pSectionHeader->Name, IMAGE_SIZEOF_SHORT_NAME, sectionName, IMAGE_SIZEOF_SHORT_NAME);
-#elif __linux__
-	memcpy(pSectionHeader->Name, sectionName, IMAGE_SIZEOF_SHORT_NAME);
-#else
-#error "OS not supported"
-#endif
+	std::memcpy(pSectionHeader->Name, sectionName, IMAGE_SIZEOF_SHORT_NAME);
 	pSectionHeader->Misc.VirtualSize = dwSectionDataSize;
 	pSectionHeader->VirtualAddress = AlignUp(pLastSectionHeader->VirtualAddress + pLastSectionHeader->Misc.VirtualSize,
 		pNtHeader->OptionalHeader.SectionAlignment);
@@ -180,17 +223,25 @@ PIMAGE_SECTION_HEADER AddSection(std::fstream& hFile, unsigned char *sectionData
 	pSectionHeader->NumberOfLinenumbers = 0;
 	pSectionHeader->Characteristics = 0xE0000020;
 
-	// Position the file pointer where the new section header should be written
-	hFile.seekg(dwFstSectionHeaderOffset + IMAGE_SIZEOF_SECTION_HEADER*pNtHeader->FileHeader.NumberOfSections, std::ios::beg);
+	try
+	{
+		// Position the file pointer where the new section header should be written
+		hFile.seekg(dwFstSectionHeaderOffset + IMAGE_SIZEOF_SECTION_HEADER*pNtHeader->FileHeader.NumberOfSections, std::ios::beg);
 
-	// Write the new section header
-	hFile.write((char*)pSectionHeader, IMAGE_SIZEOF_SECTION_HEADER);
+		// Write the new section header
+		hFile.write((char*)pSectionHeader, IMAGE_SIZEOF_SECTION_HEADER);
 
-	// Position the file pointer to where the section data should be written
-	hFile.seekg(pSectionHeader->PointerToRawData, std::ios::beg);
+		// Position the file pointer to where the section data should be written
+		hFile.seekg(pSectionHeader->PointerToRawData, std::ios::beg);
 
-	// Write the section data
-	hFile.write((char*)sectionData, pSectionHeader->SizeOfRawData);
+		// Write the section data
+		hFile.write((char*)sectionData, pSectionHeader->SizeOfRawData);
+	}
+	catch (std::fstream::failure e)
+	{
+		std::cerr << "AddSection: Error occured while writing new section with data to file: " << e.what() << std::endl;
+		return nullptr;
+	}
 
 	// Update the PE header
 	pNtHeader->FileHeader.NumberOfSections++;
@@ -202,8 +253,17 @@ PIMAGE_SECTION_HEADER AddSection(std::fstream& hFile, unsigned char *sectionData
 
 BOOL WriteDataToFile(std::ofstream& hFile, DWORD dwOffset, DWORD dwSize, BYTE* data)
 {
-	hFile.seekp(dwOffset, std::ios::beg);
-	hFile.write((char*) data, dwSize);
+	try
+	{
+		hFile.seekp(dwOffset, std::ios::beg);
+		hFile.write((char*)data, dwSize);
+	}
+	catch (std::fstream::failure e)
+	{
+		std::cerr << "WriteDataToFile: Error while writing data to file: " << e.what() << std::endl;
+		return FALSE;
+	}
+	
 
 	return TRUE;
 }
@@ -217,9 +277,11 @@ BYTE* LoadExecutableSection(std::fstream& hFile, PIMAGE_DOS_HEADER pDosHeader, P
 	for (WORD i = 0; i < pNtHeader->FileHeader.NumberOfSections; ++i)
 	{
 		*pSectionHeader = (PIMAGE_SECTION_HEADER)ReadHeader(hFile, IMAGE_SIZEOF_SECTION_HEADER, dwFstSctHdrOffset + i*IMAGE_SIZEOF_SECTION_HEADER);
-		if (pSectionHeader == nullptr)
+		if (*pSectionHeader == nullptr)
 		{
-			return nullptr;
+			std::cerr << "LoadExecutableSection: Invalid section header read, offset: " <<
+				dwFstSctHdrOffset + i*IMAGE_SIZEOF_SECTION_HEADER << std::endl;
+			continue;
 		}
 
 		//if (((*pSectionHeader)->Characteristics & 0x00000020) && ((*pSectionHeader)->Characteristics & 0x20000000))
@@ -236,8 +298,17 @@ BYTE* LoadExecutableSection(std::fstream& hFile, PIMAGE_DOS_HEADER pDosHeader, P
 	}
 
 	if (*pSectionHeader == nullptr)
+	{
+		std::cerr << "LoadExecutableSection: Error while reading executable section header" << std::endl;
 		return nullptr;
+	}
+
 	sectionData = LoadSection(hFile, *pSectionHeader);
+	if (sectionData == nullptr)
+	{
+		std::cerr << "LoadExecutableSection: Unable to load section with executable code to memory" << std::endl;
+		return nullptr;
+	}
 
 	return sectionData;
 }
@@ -262,12 +333,20 @@ BYTE* ExtractOverlays(std::fstream& hFile, PIMAGE_SECTION_HEADER pLastSectionHea
 	DWORD begin, end;
 	unsigned char *overlayBuffer;
 
-	// Get the file size
-	hFile.seekg(0, std::ios::beg);
-	begin = (DWORD)hFile.tellg();
-	hFile.seekg(0, std::ios::end);
-	end = (DWORD)hFile.tellg();
-	fileSize = end - begin;
+	try
+	{
+		// Get the file size
+		hFile.seekg(0, std::ios::beg);
+		begin = (DWORD)hFile.tellg();
+		hFile.seekg(0, std::ios::end);
+		end = (DWORD)hFile.tellg();
+		fileSize = end - begin;
+	}
+	catch (std::fstream::failure e)
+	{
+		std::cerr << "ExtractOverlays: Error while calculating file size: " << e.what() << std::endl;
+		return nullptr;
+	}
 
 	// Position where the overlay starts
 	DWORD overlayStart = pLastSectionHeader->PointerToRawData + pLastSectionHeader->SizeOfRawData;
@@ -285,16 +364,24 @@ BYTE* ExtractOverlays(std::fstream& hFile, PIMAGE_SECTION_HEADER pLastSectionHea
 	// Calculate overlay size and allocate memory for buffer
 	overlaySize = fileSize - overlayStart;
 	overlayBuffer = (unsigned char *)malloc(overlaySize * sizeof(unsigned char));
-	if (overlayBuffer == NULL)
+	if (overlayBuffer == nullptr)
 	{
-		std::cerr << "Overlay: Insufficient memory for overlay buffer!" << std::endl;
+		std::cerr << "ExtractOverlays: Insufficient memory for overlay buffer!" << std::endl;
 		std::cerr << "----------------------------------------" << std::endl;
 		return nullptr;
 	}
 
-	// Read the overlay into the buffer
-	hFile.read((char*)overlayBuffer, overlaySize);
+	try
+	{
+		// Read the overlay into the buffer
+		hFile.read((char*)overlayBuffer, overlaySize);
+	}
+	catch (std::fstream::failure e)
+	{
+		std::cerr << "ExtractOverlays: Error while reading overlay to memory: " << e.what() << std::endl;
+		return nullptr;
+	}
+	
 	*overlay_size = overlaySize;
-
 	return overlayBuffer;
 }
